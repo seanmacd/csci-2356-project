da7e0e3bb01e1592bf827e649aa8318ed230744c
Wenda <no@no.com>
2025-03-08 18:11:00 -0400

    button

--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -1,2 +1,8 @@
-
 {/* Create button component Wenda*/}
+
+export function Button()
+{
+  return (
+    <button className="bg-green-500 p-4 border-4 border-green-950 rounded-2xl hover:bg-green-700" onClick={() => window.location.href = '/'}> Play Again </button>
+  )
+}
--- ui/src/pages/leafs/YellowBirch.tsx
+++ ui/src/pages/leafs/YellowBirch.tsx
@@ -1,5 +1,6 @@
 import fox_tree_logo from '../../assets/fox_tree_logo.png'
 import './styles.css'
+import {Button} from '../../components/Button'
 
 export function YellowBirchLeafPage() {
   return (<div>
@@ -10,7 +11,7 @@ export function YellowBirchLeafPage() {
   <main>
     <div className="box-border size 32 border-2 py-6 px-8 bg-amber-200 text-center text-black text-3xl">Yellow Birch</div>
     <div className="flex justify-center"><img className="fox-tree-logo" src={fox_tree_logo} alt="Temporary Birch Tree Picture" /></div>
-    <div className="flex justify-center"><button className="bg-green-500 p-4 border-4 border-green-950 rounded-2xl hover:bg-green-700" onClick={() => window.location.href = '/'}> Play Again </button></div>
+    <div className="flex justify-center"><Button/></div>
   </main>
 
   </div>)
