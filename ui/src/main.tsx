97adcc2b0d31509df16a0fa948cfe09fbef50954
= <s3anmacd@gmail.com>
2025-03-08 15:18:58 -0400

    Added router

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Link, Outlet, useParams } from 'react-router'
+
 import './index.css'
-import App from './App.tsx'
+import { Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+function Layout() {
+  return (
+    <>
+      <div className='min-h-screen'> 
+        <Outlet />
+      </div>
+      <footer className='bg-emerald-800 text-slate-50 p-4 text-center'>The Thinkers Solutions Inc. 2025</footer>
+    </>
+  )
+}
+
+function IdPage() {
+  const { id } = useParams<{ id: string }>()
+
+  const map: Record<string, string> = {
+    1: 'foo',
+    2: 'bar',
+    3: 'baz',
+  }
+
+  if (!id) {
+    return null
+  }
+
+  const message = map[id]
+
+  return <div className="bg-green-500 text-slate-50">{message} page!</div>
+}
---
+++ ui/src/pages/HomePage.tsx
@@ -0,0 +1,3 @@
+export function HomePage() {
+  return <div className="bg-slate-900 text-slate-50">Home page!</div>
+}
