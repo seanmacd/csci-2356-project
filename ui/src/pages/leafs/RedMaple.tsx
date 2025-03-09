439431621e18b42833c02a35dd895d4a3327414d
con <no@no.com>
2025-03-08 20:24:53 -0400

    Created LeafImage component

--- ui/src/pages/leafs/RedMaple.tsx
+++ ui/src/pages/leafs/RedMaple.tsx
@@ -1,3 +1,12 @@
-export function RedMapleLeafPage() {
-  return <div>Red maple</div>
+//Connor's Page
+//import foxTree from '../../assets/fox_tree_logo.png';
+import redMaple from '../../assets/RedMapleTESTER.png';
+import {LeafImage} from '../../components/LeafImage';
+
+export function RedMapleLeafPage() { 
+  return (
+    <div className="flex justify-center">
+      <LeafImage src={redMaple} alt="Red Maple leaf" />
+    </div>
+  );
 } 
