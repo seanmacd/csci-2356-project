4737daa77ae2989d380aeb78928eea9a041803c0
= <s3anmacd@gmail.com>
2025-03-16 22:39:42 -0300

    merge for main

--- ui/src/pages/leafs/RedOak.tsx
+++ ui/src/pages/leafs/RedOak.tsx
@@ -1,22 +1,24 @@
 // sean's page
-import redOakPhotoroom from '@/assets/leafPhotos/Red-Oak-Photoroom.png'
-import {PlayAgainButton, Header, LeafImage, TangoText} from '@/components'
-import { LeafNameHeader } from '@/components/LeafNameHeader'
 import Redoak from '@/assets/audio/redoak.mp3'
+import redOakPhotoroom from '@/assets/leafPhotos/Red-Oak-Photoroom.png'
+import {Header, LeafImage, PlayAgainButton, TangoText} from '@/components'
+import {LeafNameHeader} from '@/components/LeafNameHeader'
 
 export function RedOakLeafPage() {
   return (
     <>
       <title>Red Oak</title>
-      <Header />
-      <LeafNameHeader name="Red Oak" />
-      <div className="flex justify-center">
-        <LeafImage src={redOakPhotoroom} alt="Image of red oak leaf" />
+      <div className="max-w-3xl mx-auto">
+        <Header />
+        <LeafNameHeader name="Red Oak" />
+        <div className="flex justify-center">
+          <LeafImage src={redOakPhotoroom} alt="Image of red oak leaf" />
+        </div>
+        <div className="flex justify-center">
+          <PlayAgainButton />
+        </div>
+        <TangoText text="This is a red oak tree leaf, in the fall they turn red." audio={Redoak} />
       </div>
-      <div className="flex justify-center">
-        <PlayAgainButton />
-      </div>
-      <TangoText text="This is a red oak tree leaf, in the fall they turn red." audio={Redoak} />
     </>
   )
 }
