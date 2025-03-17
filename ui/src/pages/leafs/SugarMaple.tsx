632d86a718b7794d2b85d4c094adf79a3820e456
zhubaohi <91812540+zhubaohi@users.noreply.github.com>
2025-03-17 05:13:17 -0300

    SugarMaple.tsx

--- ui/src/pages/leafs/SugarMaple.tsx
+++ ui/src/pages/leafs/SugarMaple.tsx
@@ -1,3 +1,33 @@
-export function SugarMapleLeafPage() {
-  return <div>Sugar maple</div>
+//Rongkun's Page
+
+import YellowBirch from '@/assets/audio/YellowBirchAudio.mp3'
+import yellow_birch_cartoon from '@/assets/leafPhotos/YellowBirch_Cartoon.png'
+import {PlayAgainButton, TangoText} from '@/components'
+import {LeafNameHeader} from '@/components/LeafNameHeader'
+
+import './styles.css'
+
+export function YellowBirchLeafPage() {
+  return (
+    <div className="body_background">
+      <title>Yellow Birch Game</title>
+      <LeafNameHeader name="Yellow Birch" />
+      <main>
+        <div className="flex justify-center spacing">
+          <img className="fox-tree-logo" src={yellow_birch_cartoon} alt="yellow_birch_cartoon.png picture" />
+        </div>
+
+        <div className="flex justify-center spacing button_spacing">
+          <PlayAgainButton />
+        </div>
+
+        <div className="spacing">
+          <TangoText
+            text="This is a Yellow Birch, they've adapted to live in our salty climates!"
+            audio={YellowBirch}
+          />
+        </div>
+      </main>
+    </div>
+  )
 }
