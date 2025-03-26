fd0184f930680f8296215a678d30f414002eb7b2
Wenda <no@no.com>
2025-03-26 14:02:46 -0300

    larch page bugs

--- ui/src/pages/leafs/Larch.tsx
+++ ui/src/pages/leafs/Larch.tsx
@@ -1,24 +1,49 @@
-//Rongkun Page2 
+// Rongkun Page2 
+// Modified by: Wenda, adjusted for new DrawBugs component. 
 import larchAudio from '@/assets/audio/larch.mp3'
 import larchPhoto from '@/assets/leafPhotos/Larch_Cartoon.png'
-import {LeafImage, PlayAgainButton, TangoText} from '@/components'
+import {PlayAgainButton, TangoText} from '@/components'
 import {LeafNameHeader} from '@/components/LeafNameHeader'
+import DrawBugs from '@/components/DrawBugs'
+
+const bugPositions: [number, number][] = [
+  [230, 25],
+  [290, 100],
+  [313, 185],
+  [345, 310],
+  [115, 35],
+  [10, 65],
+  [25, 210],
+  [125, 310],
+  [125, 310],
+]
 
 export function LarchLeafPage() {
     return (
       <>
         <title>Larch</title>
-        <div className="max-w-3xl mx-auto">
-          <LeafNameHeader name="Larch" />
+        <LeafNameHeader name="Larch" />
+        <main>
           <div className="flex justify-center">
-            <LeafImage src={larchPhoto} alt="Larch_Cartoon.png photo" />
+            <DrawBugs
+              canvasId="larch"
+              leafImageSrc={larchPhoto}
+              bugPositions={bugPositions}
+            />
           </div>
           <div className="flex justify-center">
             <PlayAgainButton />
           </div>
-          <TangoText text="This is a larch leaf! Unlike pine trees, larch trees lose their needles in the fall!" audio={larchAudio} />
-        </div>
-      </>
+        </main>
+        <aside>
+          <div>
+            <TangoText
+              text="This is a larch leaf! Unlike pine trees, larch trees lose their needles in the fall."
+              audio={larchAudio}
+            />
+          </div>
+        </aside>
+    </>
     )
   }
 
